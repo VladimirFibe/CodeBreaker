4cc7426813b741f54208fa9f4a30ffe0ad4e75cb
Vladimir Fibe <motiw@me.com>
2025-11-28 09:24:50 +0500

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Vladimir Fibe on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
